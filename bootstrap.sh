4de9450075de9079fd16f11a25c1f92528c6cdcc
jemamena <34324193+jemamena@users.noreply.github.com>
2023-09-19 13:59:07 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,93 @@
 #!/bin/bash
 
 clear
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root" 
+
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <operating system: L=linux, W=windows, A=Apple> <type: T=trader, S=server>"
   exit 1
 fi
 
-# Check if the 'box_type' argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <operating system: L=linux, W=windows, A=Apple>"
+# Assign the first argument to 'os' and the second argument to 'type'
+os="$1"
+type="$2"
+
+if [ "$os" == "L" ]; then
+  if [ "$type" == "T" ]; then
+    username="beanstalk"
+  if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 
+    exit 1
+  fi
+  
+  # Define the sudoers file path
+  sudoers_file="/etc/sudoers.d/"$username"_mount"
+  
+  # Check if the sudoers file already exists
+  if [ ! -f "$sudoers_file" ]; then
+      # If it doesn't exist, create the sudoers file
+      temp_sudoers=$(mktemp)
+  
+      # Add the current user to the sudoers file for the mount command
+      echo "$username ALL=(ALL) NOPASSWD: /bin/mount" > "$temp_sudoers"
+      
+  
+      # Set the permissions of the temporary file
+      chmod 0440 "$temp_sudoers"
+  
+      # Move the temporary file to the sudoers.d directory
+      mv "$temp_sudoers" "$sudoers_file"
+  
+      echo "Passwordless mount setup for user $username..."
+  fi
+  # echo
+
+
+  ## make apt-get to be used without password
+  
+  # Define the sudoers file path
+  sudoers_file="/etc/sudoers.d/"$username"_apt-get"
+  
+  # Check if the sudoers file already exists
+  if [ ! -f "$sudoers_file" ]; then
+      # If it doesn't exist, create the sudoers file
+      temp_sudoers=$(mktemp)
+  
+      # Add the current user to the sudoers file for the mount command
+      echo "$username ALL=(ALL) NOPASSWD: /usr/bin/apt-get" > "$temp_sudoers"
+      
+  
+      # Set the permissions of the temporary file
+      chmod 0440 "$temp_sudoers"
+  
+      # Move the temporary file to the sudoers.d directory
+      sudo mv "$temp_sudoers" "$sudoers_file"
+      echo "Passwordless apt-get setup for user $username..."
+  
+  ## don't ask for password when using sudo reboot
+  # Check if the sudoers file already exists
+  if [ ! -f /etc/sudoers.d/nopsd ]; then
+      # If it doesn't exist, create the sudoers file
+      echo "$username ALL=(ALL) NOPASSWD: /sbin/reboot" > $HOME/nopsd
+  
+      # Now copy this file to /etc/sudoers.d/
+      cp $HOME/nopsd /etc/sudoers.d/
+  
+      # Set the correct permissions for the sudoers file
+      chmod 0440 /etc/sudoers.d/nopsd
+  
+      # Remove the temporary file
+      rm $HOME/nopsd
+  
+      echo "password setup so no need for sudo..."
+  fi
+  echo
+elif [ "$type" == "S" ]; then
+  username="root"
+else
+  echo "Invalid type: $type"
   exit 1
 fi
-trader_type="$1"
 
 
 # Ask user for their GitHub token without displaying it
@@ -34,178 +110,38 @@ else
     exit 1
 fi
 
-# # Validate the token
-# echo "Validating GitHub token..."
-# validation_result=$(curl -s -H "Authorization: token $github_token" https://api.github.com/user/orgs)
 
-# if [[ "$validation_result" == *"TruMeter-Cloud"* ]]; then
-#     echo "Token is valid."
-#     # You can now use $github_token in your script
-# else
-#     echo "Token is invalid or does not have access to TruMeter-Cloud."
-#     exit 1
-# fi
-
-
-username="beanstalk"
-
-## don't ask for password when using sudo reboot
-
-# Check if the sudoers file already exists
-if [ ! -f /etc/sudoers.d/nopsd ]; then
-    # If it doesn't exist, create the sudoers file
-    echo "$username ALL=(ALL) NOPASSWD: /sbin/reboot" > $HOME/nopsd
-
-    # Now copy this file to /etc/sudoers.d/
-    cp $HOME/nopsd /etc/sudoers.d/
-
-    # Set the correct permissions for the sudoers file
-    chmod 0440 /etc/sudoers.d/nopsd
-
-    # Remove the temporary file
-    rm $HOME/nopsd
-
-    echo "password setup so no need for sudo..."
-fi
-# echo
-
-
-## Setup read only and read-write ##
-
-# Define the sudoers file path
-sudoers_file="/etc/sudoers.d/"$username"_mount"
-
-# Check if the sudoers file already exists
-if [ ! -f "$sudoers_file" ]; then
-    # If it doesn't exist, create the sudoers file
-    temp_sudoers=$(mktemp)
-
-    # Add the current user to the sudoers file for the mount command
-    echo "$username ALL=(ALL) NOPASSWD: /bin/mount" > "$temp_sudoers"
-    
-
-    # Set the permissions of the temporary file
-    chmod 0440 "$temp_sudoers"
-
-    # Move the temporary file to the sudoers.d directory
-    mv "$temp_sudoers" "$sudoers_file"
-
-    echo "Passwordless mount setup for user $username..."
-fi
-# echo
-
-
-## make apt-get to be used without password
-
-# Define the sudoers file path
-sudoers_file="/etc/sudoers.d/"$username"_apt-get"
-
-# Check if the sudoers file already exists
-if [ ! -f "$sudoers_file" ]; then
-    # If it doesn't exist, create the sudoers file
-    temp_sudoers=$(mktemp)
-
-    # Add the current user to the sudoers file for the mount command
-    echo "$username ALL=(ALL) NOPASSWD: /usr/bin/apt-get" > "$temp_sudoers"
-    
-
-    # Set the permissions of the temporary file
-    chmod 0440 "$temp_sudoers"
-
-    # Move the temporary file to the sudoers.d directory
-    sudo mv "$temp_sudoers" "$sudoers_file"
-
-    echo "Passwordless apt-get setup for user $username..."
-fi
-echo
-
-
-# Prompt the user for the country code
-#read -p "Please enter your country's ISO2 code (for instance, 'UK' for United Kingdom): " COUNTRY_CODE
-
-
-# Add the country code to the file
-#echo "country=$COUNTRY_CODE" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf
-
-
-
-# # Replace 'username' with your actual username
-# username="trumeter"
-
-# # This command checks if the file exists
-# if [ ! -f "/etc/sudoers.d/$username" ]; then
-#     echo "$username ALL=(ALL:ALL) NOPASSWD:/usr/bin/apt-get" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/$username
-# fi
-# echo "Passwordless apt-get setup done"
-# echo
-
-
-# ## create and change to trumeter user ##
-
-# # Prompt the user to ask if they want to change the user
-# read -p "Do you want to change the user to 'trumeter'? (y/n): " response
-# if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-# then
-#     # User responded with 'yes' or 'y'
-    
-#     # Create the new user
-#     useradd -m trumeter
-
-#     # Prompt for the new user's password
-#     echo "Enter password for new user 'trumeter': "
-#     read password
-#     echo "trumeter:$password" | chpasswd
-
-#     # Change to the new user
-#     su - trumeter
-# else
-#     echo "User change skipped."
-# fi
 
 ## GITHUB ##
+email=jemamena@gmail.com
 
-# Capture the output of `whoami` command into a variable named "current_user"
-# current_user=trumeter
+# Generate a new SSH key
+echo "Generating a new SSH key..."
 
-# read -p "Do you want to generate a new SSH key? (y/n): " response
-# if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-# then
-  # rm /root/.ssh/id_rsa
-  # rm /root/.ssh/id_rsa.pub
-  
-  # Generate a new SSH key
-  echo "Generating a new SSH key..."
-  
-  # Ask for the email associated with the GitHub account
-  # echo "Please enter the email associated with your GitHub account:"
-  # read email
-  
-  # Ensure the user's home directory has a .ssh directory
-  sudo -u $username mkdir -p /home/$username/.ssh
-  
-  # Generate the SSH key as the trumeter user
-  # sudo -u $username ssh-keygen -t rsa -b 4096 -C "$email" -f /home/$username/.ssh/id_rsa -N ""
-  sudo -u $username ssh-keygen -t rsa -b 4096 -C "javier.ema@trumeter.com" -f /home/$username/.ssh/id_rsa -N ""
-  
-  echo "SSH key generated successfully."
-  
-  public_key=$(sudo -u $username cat /home/$username/.ssh/id_rsa.pub)
-  
-  # Start the ssh-agent and add the SSH key to it
-  sudo -u $username bash -c 'eval "$(ssh-agent -s)" && ssh-add /home/$current_user/.ssh/id_rsa'
-  
-  # Set appropriate permissions on the .ssh directory and its contents
-  chown -R $username:$username /home/$username/.ssh
-  chmod 700 /home/$username/.ssh
-  chmod 600 /home/$username/.ssh/*
-  
-  # Use the GitHub API to add the SSH key to the account
-  curl -X POST -H "Authorization: token $github_token" \
-    --data "{\"title\":\"`hostname`\",\"key\":\"$public_key\"}" \
-    https://api.github.com/user/keys
-# else
-#     echo "SSH key generation skipped"
-# fi
+
+# Ensure the user's home directory has a .ssh directory
+sudo -u $username mkdir -p /home/$username/.ssh
+
+# Generate the SSH key as the trumeter user
+# sudo -u $username ssh-keygen -t rsa -b 4096 -C "$email" -f /home/$username/.ssh/id_rsa -N ""
+sudo -u $username ssh-keygen -t rsa -b 4096 -C "javier.ema@trumeter.com" -f /home/$username/.ssh/id_rsa -N ""
+
+echo "SSH key generated successfully."
+
+public_key=$(sudo -u $username cat /home/$username/.ssh/id_rsa.pub)
+
+# Start the ssh-agent and add the SSH key to it
+sudo -u $username bash -c 'eval "$(ssh-agent -s)" && ssh-add /home/$current_user/.ssh/id_rsa'
+
+# Set appropriate permissions on the .ssh directory and its contents
+chown -R $username:$username /home/$username/.ssh
+chmod 700 /home/$username/.ssh
+chmod 600 /home/$username/.ssh/*
+
+# Use the GitHub API to add the SSH key to the account
+curl -X POST -H "Authorization: token $github_token" \
+  --data "{\"title\":\"`hostname`\",\"key\":\"$public_key\"}" \
+  https://api.github.com/user/keys
 
 
 
@@ -224,6 +160,6 @@ echo
 echo "Executing setup.sh..."
 
 chmod +x setup.sh
-#echo "su" $username "-c ./setup.sh" $box_type
-su "$username" -c "/home/$username/setup.sh $trader_type"
+
+su "$username" -c "/home/$username/setup.sh $os $type"
 
